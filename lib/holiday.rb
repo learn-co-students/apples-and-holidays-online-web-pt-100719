f2280a6aaced6220a8836f566229aec4bd590260
Derric Fragosa <fderric@gmail.com>
2019-10-26 22:30:46 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,10 +25,19 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
+holiday_supplies.each do |season, holidays| 
+  
+ puts "#{season.capitalize}:"
+  
+ holidays.each do |holiday, supplies|
+
+     puts "  #{holiday.to_s.split('_').map{|w| w.capitalize}.join(' ')}: #{supplies.join(", ")}"
  
+    end 
+ end 
 end
 
-def all_holidays_with_bbq(holiday_hash)
+# def all_holidays_with_bbq(holiday_hash)
 
 
-end
+# end
